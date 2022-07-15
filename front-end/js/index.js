ca6e2c6ffa613b7ddefbc476e3af52426edcd86a
Lucas Santana <lucas.diassantana@gmail.com>
2022-07-14 21:09:42 -0300

    first commit

---
+++ front-end/js/index.js
@@ -0,0 +1,25 @@
+function validarCPF(event, form) {
+    event.preventDefault();
+    const inputCpf = form.cpf;
+    if (inputCpf) {
+        const cpf = inputCpf.value;
+        if (cpf.length === 11) {
+            const URL = `http://localhost/validar-cpf/${cpf}`;
+            axios.request(URL)
+                .then(resposta => mostrarResposta(resposta.data))
+                .catch(erro => console.error(erro))
+        }
+
+    }
+}
+
+function mostrarResposta(data) {
+    const respostaDiv = document.querySelector('.resposta')
+    const mensagem = `
+    CPF: ${data.cpf}<br>
+    Status: ${data.status}<br>
+    Unidade Federativa: ${data.unidadeFederativa}
+    `
+    respostaDiv.innerHTML = `<h2>Resposta:</h2><br>${mensagem}`;
+    console.log(data)
+}
---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const port = 80;
+
+const validarCPF = require('../lib/verificador_cpf')
+
+app.get("/validar-cpf/:cpf", (req, res) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    let cpf = req.params.cpf
+    let status = validarCPF.verificarCPF(cpf)
+    let unidadeFederativa = status ? validarCPF.buscarUnidadeFederativa(cpf) : "-"
+
+    res.json({
+        cpf: cpf,
+        status: status,
+        unidadeFederativa: unidadeFederativa
+    })
+})
+
+app.listen(port, () => {
+    console.log('Servidor escutando na porta ' + port)
+})
